f4f461bb7c2da7e38727f9a0ee6450a694abcedc
nathanielmuci <nate.muci@gmail.com>
2022-11-27 11:24:56 -0600

    added bootstrap styling to employee cards

--- index.js
+++ index.js
@@ -79,24 +79,29 @@ function createEmp() {
 function addHtml(){
     const layout = 
     `<!DOCTYPE html>
-                 <html lang="en">
-                <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-                </head>
-                <body>
-                <header>
-                <div class="jumbotron jumbotron-fluid" style="background-color: salmon;">
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+    
+    <body>
+        <header>
+            <div class="jumbotron jumbotron-fluid" style="background-color: salmon;">
                 <div class="container">
-                <h1 class="display-4">My Team</h1>
-                </header>
-
-                   
-            
-                </div>`
+                    <h1 class="display-4">My Team</h1>
+                </div>
+            </div>    
+        </header>
+        <div class='container'>
+            <div class="row row-cols-2 row-cols-lg-4 g-2 g-lg-3">
+                
+                    `
                 ;
     fs.appendFile("profile.html", layout, function (err){
         if (err){
@@ -113,45 +118,57 @@ function memHtml(emp){
         let layout = "";
         if (role === 'Engineer'){
             const gitHub = emp.getGithub();
-            layout =  `<div class="card" style="width: 18rem; ">
-            <div class="card-body" style="background-color: blue; color:white;">
-            <h3 class="card-title">${name}</h3>
-            <h4>${role}</h4>
-            </div>
-            <div class="card-body">
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">Id: ${id}</li>
-            <li class="list-group-item">Email: ${email}</li>
-            <li class="list-group-item">${gitHub}</li>
-            </ul>
+            layout =  
+            `<div class="col">
+                <div class="card" style="width: 18rem; ">
+                    <div class="card-body" style="background-color: blue; color:white;">
+                        <h3 class="card-title">${name}</h3>
+                        <h4>${role}</h4>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Id: ${id}</li>
+                            <li class="list-group-item">Email: ${email}</li>
+                            <li class="list-group-item">GitHub username: ${gitHub}</li>
+                        </ul>
+                    </div>
+                </div>
             </div>`;
         } else if (role === 'Intern'){
             const school = emp.getSchool();
-            layout =  `<div class="card" style="width: 18rem; ">
-            <div class="card-body" style="background-color: blue; color:white;">
-            <h3 class="card-title">${name}</h3>
-            <h4>${role}</h4>
-            </div>
-            <div class="card-body">
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">Id: ${id}</li>
-            <li class="list-group-item">Email: ${email}</li>
-            <li class="list-group-item">${school}</li>
-            </ul>
+            layout =  
+            `<div class="col">
+                <div class="card" style="width: 18rem; ">
+                    <div class="card-body" style="background-color: blue; color:white;">
+                        <h3 class="card-title">${name}</h3>
+                        <h4>${role}</h4>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Id: ${id}</li>
+                            <li class="list-group-item">Email: ${email}</li>
+                            <li class="list-group-item">GitHub username: ${school}</li>
+                        </ul>
+                    </div>
+                </div>
             </div>`;
         } else {
             const officeNum = emp.getOfficenum();
-            layout =  `<div class="card" style="width: 18rem; ">
-            <div class="card-body" style="background-color: blue; color:white;">
-            <h3 class="card-title">${name}</h3>
-            <h4>${role}</h4>
-            </div>
-            <div class="card-body">
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">Id: ${id}</li>
-            <li class="list-group-item">Email: ${email}</li>
-            <li class="list-group-item">${officeNum}</li>
-            </ul>
+            layout =  
+            `<div class="col">
+                <div class="card" style="width: 18rem; ">
+                    <div class="card-body" style="background-color: blue; color:white;">
+                        <h3 class="card-title">${name}</h3>
+                        <h4>${role}</h4>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Id: ${id}</li>
+                            <li class="list-group-item">Email: ${email}</li>
+                            <li class="list-group-item">GitHub username: ${officeNum}</li>
+                        </ul>
+                    </div>
+                </div>
             </div>`
         }
         fs.appendFile('profile.html', layout, function (err){
@@ -163,7 +180,11 @@ function memHtml(emp){
         });
     };
 function endApp(){
-    const eHtml = `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    const eHtml = 
+    `       </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
     </body>
     </html>`;
     fs.appendFile('profile.html', eHtml, function (err){
